a52bcdf194758daceb699adc3f128c0ce772fee6
Saranya Krishnakumar <saranya_k@apple.com>
2024-11-12 13:27:17 -0800

    Add authorization

---
+++ server/src/main/java/org/apache/cassandra/sidecar/accesscontrol/authorization/Permission.java
@@ -0,0 +1,11 @@
+package org.apache.cassandra.sidecar.accesscontrol.authorization;
+
+public class Permission
+{
+    // STREAM
+    // UPLOAD
+    // SNAPSHOT
+    // CLEAR_SNAPSHOT
+    // SCHEMA
+    //
+}
---
+++ server/src/main/java/org/apache/cassandra/sidecar/accesscontrol/authorization/Resource.java
@@ -0,0 +1,5 @@
+package org.apache.cassandra.sidecar.accesscontrol.authorization;
+
+public class Resource
+{
+}
